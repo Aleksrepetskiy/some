1b56d6581bd3eaf36b00d5eb6ec4afea9af02930
Aleksrepetskiy <Aleksrepetskiy@mail.ru>
2015-12-26 16:15:41 +0200

    dz3-4. done

--- GoIT/javascript/home/js_03-04/js/script.js
+++ GoIT/javascript/home/js_03-04/js/script.js
@@ -1,3 +1,16 @@
+DOMCreator = function() {};
+DOMCreator.createElement = function(tag, parent, className, innerHTML) {
+  var elem = document.createElement(tag);
+  parent.appendChild(elem);
+  if (className) {
+    elem.className = className;
+  }
+  if (innerHTML) {
+    elem.innerHTML = innerHTML;
+  }
+  return elem;
+};
+
 window.onload = function() {
   // var div = document.createElement('div');
   // div.className = 'header';
@@ -17,6 +30,16 @@ window.onload = function() {
     }
     return elem;
   };
+
+  DOMCreator.createCheckBox = function (parent, text) {
+    var div = DOMCreator.createElement('div', parent, 'checkbox' );
+    var label = DOMCreator.createElement('label', div );
+    var input = DOMCreator.createElement('input', label );
+    input.type = 'checkbox';
+    DOMCreator.createElement('p', label, '', text );
+  }
+
+
   var div = DOMCreator.createElement('div', document.body, 'header');
   DOMCreator.createElement('h3', div, '', 'Тест по программированию');
   var content = DOMCreator.createElement('ul', document.body, 'content');
@@ -25,7 +48,12 @@ window.onload = function() {
     DOMCreator.createElement('h4', li1, '', i + '. Вопрос №' + i);
     var ul = DOMCreator.createElement('ul', li1);
     for (var j = 1; j <= 3; j++) {
-      DOMCreator.createElement('li', ul);
+      var li2 = DOMCreator.createElement('li', ul);
+      DOMCreator.createCheckBox(li2, "Вариант ответа №" + j)
+
     }
   }
+  var button = DOMCreator.createElement ('div', document.body, 'button');
+  var btn = DOMCreator.createElement ('button', button, 'btn btn-primary', 'Проверить результат');
+
 };
