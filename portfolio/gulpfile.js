97a3b372f5c5fc1a9617b3e22cd7871e24d9fdd1
Aleksrepetskiy <Repetskiyaleksandr@gmail.com>
2017-09-07 17:05:31 +0300

    add style gulp

--- portfolio/gulpfile.js
+++ portfolio/gulpfile.js
@@ -1,12 +1,21 @@
 const gulp = require('gulp');
 const pug  = require('gulp-pug');
 
+//styles
+const sass          = require('gulp-sass');
+const rename        = require('gulp-rename');
+const sourcemaps    = require('gulp-sourcemaps');
+
 const paths = {
     root: './build',
     templates: {
         pages: 'app/templates/pages/*.pug',
         src: 'app/templates/**/*.pug',
-        dest: 'build/assets/'
+        dest: 'build/'
+    },
+    styles: {
+        src: 'app/styles/**/*.scss',
+        dest: 'build/styles/'
     }
 }
 
@@ -17,4 +26,16 @@ function templates() {
         .pipe(gulp.dest(paths.root));
 }
 
+//scss
+function styles() {
+    return gulp.src('./app/styles/style.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(sourcemaps.write())
+        .pipe(rename({suffix: 'min'}))
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(gulp.dest(paths.styles.dest));
+}
+
 exports.templates = templates;
+exports.styles    = styles;
