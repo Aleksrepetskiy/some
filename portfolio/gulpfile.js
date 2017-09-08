92227e32057e1b043257b8a4a47bcfd477407182
Aleksrepetskiy <Repetskiyaleksandr@gmail.com>
2017-09-08 16:37:16 +0300

    gulpfile add mod

--- portfolio/gulpfile.js
+++ portfolio/gulpfile.js
@@ -1,5 +1,7 @@
-const gulp = require('gulp');
-const pug  = require('gulp-pug');
+const gulp          = require('gulp');
+const pug           = require('gulp-pug');
+const del           = require('del');
+const browserSync   = require('browser-sync').create();
 
 //styles
 const sass          = require('gulp-sass');
@@ -16,6 +18,10 @@ const paths = {
     styles: {
         src: 'app/styles/**/*.scss',
         dest: 'build/styles/'
+    },
+    images: {
+        src: 'app/images/**/*.*',
+        dest: 'build/images/'
     }
 }
 
@@ -25,17 +31,48 @@ function templates() {
         .pipe(pug({ pretty:true}))
         .pipe(gulp.dest(paths.root));
 }
-
 //scss
 function styles() {
     return gulp.src('./app/styles/style.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(sourcemaps.write())
-        .pipe(rename({suffix: 'min'}))
-        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(rename({suffix: '-min'}))
         .pipe(gulp.dest(paths.styles.dest));
+}
+//img
+function images(){
+    return gulp.src(paths.images.src)
+    .pipe(gulp.dest(paths.images.dest));
+}
+//clear
+function clean() {
+    return del(paths.root)
+}
+//watcher src
+function watch() {
+    gulp.watch(paths.styles.src, styles);
+    gulp.watch(paths.templates.src, templates);
+    gulp.watch(paths.images.src, images);
+}
+//browserSync
+function server() {
+    browserSync.init({
+        server: paths.root
+    });
+    browserSync.watch(paths.root + '/**/*.*', browserSync.reload );
 }
 
 exports.templates = templates;
 exports.styles    = styles;
+exports.clean     = clean;
+
+gulp.task('default', gulp.series(
+    gulp.parallel(styles, templates, images),
+    gulp.parallel(watch, server)
+));
+
+gulp.task('build', gulp.series(
+    clean,
+    gulp.parallel(styles, templates, images)
+));
