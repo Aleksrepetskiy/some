3a761aaa5da433eddf02d11bd3b41b576a6a778e
Aleksrepetskiy <Repetskiyaleksandr@gmail.com>
2017-09-29 16:49:46 +0300

    animation menu

--- portfolio/app/js/script.js
+++ portfolio/app/js/script.js
@@ -1,1 +1,13 @@
 const $ = require('jquery');
+
+// const hamburgerContainer = document.getElementById('hamburger');
+// hamburgerContainer.click(function () {
+//     console.log('click');
+// });
+
+$(document).ready(function(){
+  $("#hamburger").click(function(){
+    $(this).toggleClass("active");
+    $('#menu').toggleClass("active");
+  });
+});
